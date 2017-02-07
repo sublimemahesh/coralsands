d9e58318d2c700fd4333b68fe91303339ebf6803
sandunchathuranga <sandun0570@gmail.com>
2017-02-07 10:52:40 +0530

    link font awesome pages

--- admin/index.php
+++ admin/index.php
@@ -6,8 +6,8 @@
         <title>AdminLTE 2 | Dashboard</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../../../cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../../../cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
         <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
         <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
         <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
@@ -268,18 +268,18 @@
         </div>
 
         <script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
-        <script src="../../../code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+        <script src="code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
         <script>
             $.widget.bridge('uibutton', $.ui.button);
         </script>
         <script src="bootstrap/js/bootstrap.min.js"></script>
-        <script src="../../../cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
         <script src="plugins/morris/morris.min.js"></script>
         <script src="plugins/sparkline/jquery.sparkline.min.js"></script>
         <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
         <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
         <script src="plugins/knob/jquery.knob.js"></script>
-        <script src="../../../cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+        <script src="cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
         <script src="plugins/daterangepicker/daterangepicker.js"></script>
         <script src="plugins/datepicker/bootstrap-datepicker.js"></script>
         <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
