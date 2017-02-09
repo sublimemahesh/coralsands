48b6a2ccd1792756e4ab7ac165aa2cb7e6cb51c0
sandunchathuranga <sandun0570@gmail.com>
2017-02-09 11:47:58 +0530

    include Introduction class file

--- include.php
+++ include.php
@@ -9,3 +9,4 @@ include BASE_PATH . '/class/Room.php';
 include BASE_PATH . '/class/Upload.php';
 include BASE_PATH . '/class/User.php';
 include BASE_PATH . '/class/Slider.php';
+include BASE_PATH . '/class/Introduction.php';
